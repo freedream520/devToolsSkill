15fa0075d4b23595cb13840aa8b0f2fbd56acca9
wy2008826 <760104178@qq.com>
2017-01-23 17:15:57 +0800

    add

--- vueStack/src/components/RouterA.vue
+++ vueStack/src/components/RouterA.vue
@@ -2,6 +2,14 @@
   <div class="a-wrap">
     <h4 v-text="title"></h4>
     <dl>
+      <dt>vue</dt>
+      <dd>（饿了么教程看明白，想通透）</dd>
+      <dd></dd>
+      <dd>组件的按需加载（减少主文件的体积，模块切割，按需加载）</dd>
+      <dd>热更新（单页应用中修改代码后实现局部更新）</dd>
+      <dd>node服务和java或者php服务的共存</dd>
+    </dl>
+    <dl>
       <dt>vue-router</dt>
       <dd>router-link-active会根据router的名字匹配  /vue 和 /vuex 会同时拥有激活的类名</dd>
     </dl>
@@ -9,12 +17,18 @@
       <dt>node</dt>
       <dd>node基础（深入浅出nodejs）</dd>
       <dd>node连接mysql数据库</dd>
-      <dd>实现前后端分离，node作为作为中间服务器（渲染页面、转发请求....）</dd>
+      <dd>实现前后端分离，node作为作为中间服务器（渲染页面、转发请求....）<a href="https://segmentfault.com/a/1190000007992200" class="fc-link">教程地址</a></dd>
       <dd></dd>
     </dl>
+    <dl>
+      <dt>webpack</dt>
+      <dd><a href="https://segmentfault.com/a/1190000007892189" class="fc-link">教程地址</a></dd>
+    </dl>
+    <dl>
+      <dt>java</dt>
+      <dd>java基础，启动简单的服务器，处理请求，连接数据库，简单的sql查询等简单应用场景</dd>
+    </dl>
     <ul>
-      <li>1.java熟悉，免得被忽悠 （启动服务，返回页面和静态资源，简单的sql查询）</li>
-      <li>2.vue精通 （饿了么教程看明白，想通透）</li>
       <li>4.es6精通 （深入浅出es6）</li>
       <li>5.webpack精通 （问题收集，解决方案整理）</li>
       <li>6.单元测试的编写</li>
@@ -66,6 +80,7 @@
     }
     dd{
       line-height: pxToRem(40);
+      margin:pxToRem(10) 0;
     }
   }
   ul{
