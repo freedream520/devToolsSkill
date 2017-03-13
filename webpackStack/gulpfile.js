003904a6ddccbf910e107ce281fc86c55c1f77f8
王宇 <wangyu@wangyudeMacBook-Air.local>
2017-03-13 17:39:04 +0800

    add gulp-ftp

---
+++ webpackStack/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp=require("gulp");
+
+var remoteServer = {
+    host: '121.196.201.74',
+    remotePath: '/',
+    user: 'root',
+    pass: '123456',
+    port:14147
+};
+
+
+
+gulp.task('deploy', function () {
+    var sftp = require('gulp-sftp');
+    var _conf = remoteServer ;
+    console.log(process.cwd() + '/dist/');
+    return gulp.src(process.cwd() + '/dist/**')
+        .pipe(sftp(_conf))
+})
