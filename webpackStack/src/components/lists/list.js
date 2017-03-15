cd594add4272a10108cc28e8fe12b93effffcd16
王宇 <wangyu@wangyudeMacBook-Air.local>
2017-03-16 00:18:12 +0800

    add

--- webpackStack/src/components/lists/list.js
+++ webpackStack/src/components/lists/list.js
@@ -1,7 +1,6 @@
 import listTpl from "./list.tpl";
 import "./list.scss";
 
-export default function Lists(data){
-	this.data=data;
-	this.html=listTpl(data);
-}
+module.exports={
+	tpl:listTpl
+};
--- webpackStack/src/js/page/about/about.js
+++ webpackStack/src/js/page/about/about.js
@@ -1,12 +1,28 @@
 import "../../../style/about/about.scss";
 
-import Lists from  "../../../components/lists/list.js";
+// import listTpl from  "../../../components/lists/list.js";
+
+// $("#loadListsBtn").on("click",function(){
+// 	var html= listTpl({
+// 		dataLists:[
+// 			{name:"李明"},{name:"angle"}
+// 		]
+// 	});
+// 	$("#listsCont").html(html);
+// })
+
+// import listTpl from  "../../../components/lists/list.js";
 
 $("#loadListsBtn").on("click",function(){
-	var lists=new Lists({
-		dataLists:[
-			{name:"李明"},{name:"angle"}
-		]
+	require.ensure([],function(){
+		var aaa=require("../../../components/lists/list.js").tpl;
+		console.log(aaa);
+		var html=aaa({
+			dataLists:[
+				{name:"李明"},{name:"angle"}
+			]
+		});
+		$("#listsCont").html(html);
 	});
-	$("#listsCont").html(lists.html);
-})
+	
+},"aaa")
