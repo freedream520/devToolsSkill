0499e685f77100951d8fa5682925cf5ab49bc817
760104178@qq.com <wy1479285>
2017-04-23 19:58:03 +0800

    change something

--- reactStack/src/main.js
+++ reactStack/src/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {render} from "react-dom";
-import {Router, Route, IndexRoute, hashHistory,history} from 'react-router';
+import {Router, Route, IndexRoute, hashHistory,browserHistory,IndexRedirect } from 'react-router';
 
 
 import App from "./components/App.js";
@@ -13,13 +13,12 @@ import Login from "./components/Login.js";
 let rootElement=document.getElementById('app');
 
 render(
-  <Router>
+  <Router history={hashHistory}>
 
     <Route path="/" component={App}>
-    	<IndexRoute path="/index" component={Index} />
-    	
+    	<IndexRedirect  to="/react" />
     	<Route path="/index" component={Index} >
-    		<IndexRoute path="/react" component={ReactC} />
+    		<IndexRoute  component={ReactC} />
     		<Route path="/react" component={ReactC} />
     		<Route path="/about" component={About} />
     	</Route>
