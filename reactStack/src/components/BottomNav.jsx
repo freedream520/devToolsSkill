2fe1cd4c88c908f5e1427da32691207347698c81
760104178@qq.com <wy1479285>
2017-05-19 17:21:16 +0800

    新增

--- reactStack/src/components/BottomNav.jsx
+++ reactStack/src/components/BottomNav.jsx
@@ -1,27 +1,33 @@
 import React,{Component} from "react";
-import {Link} from "react-router-dom";
+import {Link,NavLink} from "react-router-dom";
 
 import bottomNav from "../assets/css/BottomNav.scss";
 
-
+// 使用NavLink可以高量化显示
 export default class BottomNav extends Component{
 	constructor(props){
 		super(props);
-		// this.state={
-		// 	navs:[
-		// 		{text:"react",to:"/react"},
-		// 		{text:"redux",to:"/redux"},
-		// 	]
-		// }
+		this.state={
+			navs:[
+				{to:"/",text:"首页"},
+				{to:"/index/tender",text:"理财"},
+				{to:"/index/me",text:"我的"},
+			]
+		};
 	}
 	render() {
 		console.log("match:",this.props.match);
+		var navs=this.state.navs.map(function(nav){
+			return (
+				<li>
+					<NavLink to={nav.to} activeClassName={bottomNav.active}>{nav.text}</NavLink>
+				</li>
+			);
+		});
 		return (
 			<div className={bottomNav.wraper}>
 				<ul >
-					<li><Link  to="/" >首页</Link></li>
-					<li><Link  to="/index/tender" >理财</Link></li>
-					<li><Link  to="/index/me" >我的</Link></li>
+					{navs}
 				</ul>
 			</div>
 		);
--- reactStack/src/page/Me.jsx
+++ reactStack/src/page/Me.jsx
@@ -10,6 +10,10 @@ export default class App extends Component {
 		return (
 			<div >
 			  <h1> Me!</h1>
+			  <p>1.如何异步按需加载？</p>
+			  <p>2.如何精确高亮导航？</p>
+			  <p>3.如何避免css的污染问题？</p>
+
 			  <Link to="/login">点击登录</Link>
 			</div>
 		)
