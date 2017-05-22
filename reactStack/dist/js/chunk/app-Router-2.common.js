c8373d0949e945a2cf26d8553ac7089bb06a62d9
760104178@qq.com <wy1479285>
2017-05-22 17:57:17 +0800

    路由文件的修改

--- reactStack/dist/js/chunk/app-Router-2.common.js
+++ reactStack/dist/js/chunk/app-Router-2.common.js
@@ -1,60 +1,12 @@
 webpackJsonp([2],{
 
 /***/ 269:
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
 "use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = __webpack_require__(5);
-
-var _react2 = _interopRequireDefault(_react);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var App = function (_Component) {
-	_inherits(App, _Component);
-
-	function App(props) {
-		_classCallCheck(this, App);
-
-		return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
-	}
-
-	_createClass(App, [{
-		key: "render",
-		value: function render() {
-			return _react2.default.createElement(
-				"div",
-				null,
-				_react2.default.createElement(
-					"h1",
-					null,
-					" Tender!"
-				)
-			);
-		}
-	}]);
-
-	return App;
-}(_react.Component);
-
-exports.default = App;
-;
+throw new Error("Module build failed: SyntaxError: E:/github/devToolsSkill/reactStack/src/page/Router.jsx: Unexpected token (6:1)\n\n\u001b[0m \u001b[90m 4 | \u001b[39m\n \u001b[90m 5 | \u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 6 | \u001b[39m\u001b[33m<\u001b[39m\u001b[33m!\u001b[39m\u001b[33m--\u001b[39m { path\u001b[33m:\u001b[39m \u001b[32m'/contact'\u001b[39m\u001b[33m,\u001b[39m\n \u001b[90m   | \u001b[39m \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 7 | \u001b[39m  sidebar\u001b[33m:\u001b[39m () \u001b[33m=>\u001b[39m \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m联系我们\u001b[33m!\u001b[39m\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[33m,\u001b[39m\n \u001b[90m 8 | \u001b[39m  main\u001b[33m:\u001b[39m () \u001b[33m=>\u001b[39m \u001b[33m<\u001b[39m\u001b[33mh2\u001b[39m\u001b[33m>\u001b[39m联系我们\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mh2\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 9 | \u001b[39m} \u001b[33m--\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n");
 
 /***/ })
 
 });
-//# sourceMappingURL=app-Router-2.common.js.map?6d06b9ba77a4efa91e88
+//# sourceMappingURL=app-Router-2.common.js.map?6879a81796fb10624542
--- reactStack/src/page/Router.jsx
+++ reactStack/src/page/Router.jsx
@@ -1,4 +1,13 @@
 import React,{Component} from "react";
+import {Route,Link} from "react-router-dom";
+
+
+
+<!-- { path: '/contact',
+  sidebar: () => <div>联系我们!</div>,
+  main: () => <h2>联系我们</h2>
+} -->
+
 
 export default class App extends Component {
 	constructor(props){
@@ -7,8 +16,28 @@ export default class App extends Component {
 	render() {
 		return (
 			<div >
-			  <h1> Tender!</h1>
+			  	<h1> Router!</h1>
+			  	<ul>
+			  		<li>
+			  			1.redirect
+			  		</li>
+			  		<li>
+			  			<h3>优点：</h3>
+			  			<p>
+			  				一个路由下可以渲染多个不同的Route  如既可以渲染sidebar 也可以渲染main组件  这是vue难以做到的地方   vue只有一个router-view
+			  			</p>
+			  			
+			  		</li>
+			  		<li>
+			  			<h3>缺点：</h3>
+			  			<p>
+			  				因为没有配置文件，所以父子关系是通过url进行判定  而vue-router则是通过配置文件声明父子关系，所以和url没有关系
+			  				所以子路由的路径必须嵌套在父路由下面   
+			  			</p>
+			  		</li>
+			  	</ul>
 			</div>
 		)
 	}
 };
+
