55a413f6677822737746c991851d9039ca77f2d1
wy2008826 <wy1479285>
2017-06-06 23:21:25 +0800

    add

--- nodejs/sendEmail/email.js
+++ nodejs/sendEmail/email.js
@@ -2,25 +2,28 @@
 
 var nodemailer = require("nodemailer");//发送邮件所需要的module
  
-var transport = nodemailer.createTransport("SMTP", {
-   
+var transport = nodemailer.createTransport( {
+    host: 'smtp.163.com',
+    port: 465,
+    secure: true,
     auth: {
-        user: "985774183@qq.com",
-        pass: "1479285051"
+        user: "wy760104178@163.com",
+        pass: "wy1479285"
     }
 });
  
 transport.sendMail({
-    from : "985774183@qq.com",
+    from : "wy760104178@163.com",
     to : "760104178@qq.com",
     subject: "邮件主题",
     generateTextFromHTML : true,
-    html : "&lt;p&gt;这是封测试邮件&lt;/p&gt;"
+    // text:"hello word",
+    html : "<h4>这是封测试邮件</h4>"
 }, function(error, response){
     if(error){
         console.log(error);
     }else{
-        console.log("Message sent: " + response.message);
+        console.log("Message sent: " , response);
     }
     transport.close();
 });
